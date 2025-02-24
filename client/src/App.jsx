e45b416041d6123e0d8217beaa79e1de4f39e2ef
marcusness <marcus.ness@gmail.com>
2025-02-24 20:56:32 +0000

    Add React entry point files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>Student Housing Registration</h1>
+      <h1>Student Accommodation</h1>
       <StudentRegistration />
       <PropertySearch onFilterChange={handleFilterChange} />
       <div className="property-grid">
